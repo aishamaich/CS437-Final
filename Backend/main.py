70608c14a6df8c8973145655da070c97b7641738
aishama2 <aishama2@illinois.edu>
2025-04-27 22:47:16 -0500

    server working

--- Backend/main.py
+++ Backend/main.py
@@ -3,7 +3,11 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.responses import FileResponse
 from fastapi.responses import StreamingResponse
+from fastapi.responses import Response
+from fastapi.middleware.cors import CORSMiddleware
 from pathlib import Path
+import uvicorn
+import asyncio
 
 # For the SunFounder Controller to work on your Raspberry Pi, you need to clone the SunfounderController repo: https://github.com/sunfounder/sunfounder-controller
 from sunfounder_controller import SunFounderController
@@ -17,8 +21,7 @@ sc.set_name('Picarx-001')
 sc.set_type('Picarx')
 sc.start()
 
-WIFI_HOST = "10.0.0.207"   # Change your own Raspberry Pi IP address
-WIFI_PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
+WIFI_HOST = "192.168.10.186"   # Change your own Raspberry Pi IP address
 
 # The line below allows the server to send video to the front-end that can then be displayed in an iframe
 sc.set('video','http://'+WIFI_HOST+':9000/mjpg')
@@ -30,30 +33,28 @@ Vilib.object_detect_set_model(path='/opt/vilib/detect.tflite')
 Vilib.object_detect_set_labels(path='/opt/vilib/coco_labels.txt')
 Vilib.object_detect_switch(True)
 
-def detectDogs():
+async def detectDogs():
     while True:
         if (Vilib.object_detection_list_parameter is not None and len(Vilib.object_detection_list_parameter) > 0):
             first_object = Vilib.object_detection_list_parameter[0]
             object_type = first_object.get('class_name')
-            if (object_type == 'dog' or object_type == 'cat'):
-                # If the object type we see here is a dog (or a cat, my husky was identified as a cat), then we need to send a message to the front-end over our server's socket connection
-                print(object_type)
-        sleep(1)
+            yield(f"data: {object_type}\n\n")
+        await asyncio.sleep(0.5)
         
 px = Picarx()
 
 app = FastAPI()
-
-async def get_image():
-    image_path = Path("winston.gif")
-
-    if not image_path.is_file():
-        return {"error": "image not found"}
-
-    with image_path.open("rb") as winton:
-        img_bytes = winton.read()
-    imgio = BytesIO(img_bytes)
-    imgio.seek(0)
-
-    
-    return StreamingResponse(content=imgio, media_type="image/gif")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+@app.options("/detections")
+async def get_detection_options():
+    return Response(status_code=200)
+@app.post("/detections")
+async def get_detections():
+    return StreamingResponse(content=detectDogs(), media_type="text/event-stream")
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
